61603214b569f2cf1ce7faa1690f5d7968811cf6
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-05-20 13:31:44 +0200

    Updated Button features

--- js/script.js
+++ js/script.js
@@ -2,23 +2,46 @@
 
 const min = document.querySelector('.minutes');
 const sec = document.querySelector('.seconds');
+const btn = document.querySelector('.btn-container');
 
 const getYear = new Date().getFullYear();
 document.querySelector('.year').textContent = getYear;
 
-let minCount = 0;
-let secCount = 10;
+let minCount = 19;
+let secCount = 59;
+let interval;
+let canStart = true;
 
-document.querySelector('.start').addEventListener('click', function () {
-  const interval = setInterval(() => {
-    min.textContent = minCount <= 9 ? `${0}${minCount}` : minCount;
-    sec.textContent = secCount <= 9 ? `${0}${secCount}` : secCount;
+const reset = function (m = '00', s = '00') {
+  clearInterval(interval);
+  min.textContent = m;
+  sec.textContent = s;
+  canStart = true;
+};
 
-    secCount--;
-    if (secCount < 0) {
-      minCount--;
-      secCount = 59;
-      if (minCount < 0) clearInterval(interval);
+btn.addEventListener('click', function (e) {
+  if (e.target.classList.contains('start')) {
+    e.target.classList.add('hidden');
+    document.querySelector('[data-state="stop"]').classList.remove('hidden');
+    if (btn.classList.contains('hidden')) btn.classList.toggle('hidden');
+    if (canStart) {
+      canStart = false;
+      console.log('clicked');
+      interval = setInterval(() => {
+        min.textContent = minCount <= 9 ? `${0}${minCount}` : minCount;
+        sec.textContent = secCount <= 9 ? `${0}${secCount}` : secCount;
+
+        secCount--;
+        if (secCount < 0) {
+          minCount--;
+          secCount = 59;
+          if (minCount < 0) reset();
+        }
+      }, 1000);
     }
-  }, 1000);
+  } else {
+    e.target.classList.add('hidden');
+    document.querySelector('[data-state="start"]').classList.remove('hidden');
+    reset('20');
+  }
 });
