4581b3fbe3380da901f1d50851d0c95b15799fc3
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-05-20 23:54:25 +0200

    Feature: Progress bar implemented.

--- js/script.js
+++ js/script.js
@@ -3,29 +3,42 @@
 const min = document.querySelector('.minutes');
 const sec = document.querySelector('.seconds');
 const btn = document.querySelector('.btn-container');
+const circle = document.querySelector('circle');
 
-const getYear = new Date().getFullYear();
-document.querySelector('.year').textContent = getYear;
+// Circle progress bar
+const radius = 100;
+const circumference = 2 * Math.PI * radius;
+console.log(circumference);
+circle.style.strokeDasharray = circumference;
+circle.style.strokeDashoffset = circumference;
+
+// Dynamic full year
+document.querySelector('.year').textContent = new Date().getFullYear();
 
 let minCount = 19;
 let secCount = 59;
+let isRunning = true;
 let interval;
-let canStart = true;
 
-const reset = function (m = '00', s = '00') {
+const reset = function (initialMin = '00', initialSec = '00') {
   clearInterval(interval);
-  min.textContent = m;
-  sec.textContent = s;
-  canStart = true;
+  min.textContent = initialMin;
+  sec.textContent = initialSec;
+  circle.classList.remove('animate');
+  isRunning = true;
+  minCount = 19;
+  secCount = 59;
 };
 
 btn.addEventListener('click', function (e) {
   if (e.target.classList.contains('start')) {
     e.target.classList.add('hidden');
     document.querySelector('[data-state="stop"]').classList.remove('hidden');
-    if (btn.classList.contains('hidden')) btn.classList.toggle('hidden');
-    if (canStart) {
-      canStart = false;
+    // if (btn.classList.contains('hidden')) btn.classList.toggle('hidden');
+
+    if (isRunning) {
+      isRunning = false;
+      circle.classList.add('animate');
       console.log('clicked');
       interval = setInterval(() => {
         min.textContent = minCount <= 9 ? `${0}${minCount}` : minCount;
